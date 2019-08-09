66ecc998fbf6d33c8bf8053ce39fa29220a958f1
taoye <2585469768@qq.com>
2019-08-09 17:21:35 +0800

    读取html文件 并显示静态资源

--- 03.htmlTem.js
+++ 03.htmlTem.js
@@ -3,6 +3,13 @@ const app = new Koa();
 
 const fs = require('fs');
 
+// 读取静态文件
+const path = require('path');
+const serve = require('koa-static');
+
+const staticFile = serve(path.join(__dirname));
+app.use(staticFile);
+
 const main = ctx => {
   ctx.response.type = 'html';
   ctx.response.body = fs.createReadStream('./html/01.html');
